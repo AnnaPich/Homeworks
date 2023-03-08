0274d93c19c0a334483b1e76017cf2d38d3c9e05
Anna <anek_ogonek@mail.ru>
2023-03-08 17:52:51 +0300

    added task_22

---
+++ Homework_4/Task_22.py
@@ -0,0 +1,23 @@
+# Задача 22:
+# Даны два неупорядоченных набора целых чисел (может быть с повторениями).
+# Выдать без повторений в порядке возрастания все те числа, которые встречаются в обоих наборах.
+# Пользователь вводит 2 списка. 1 строка - первый список через пробел. 2 строка - второй список через пробел.
+
+list_1 = input("Enter a first list of integers separated by a space: ")
+list_2 = input("Enter a second list of integers separated by a space: ")
+
+set_1 = set(list_1.split())
+set_2 = set(list_2.split())
+set_3 = set_1.union(set_2)
+print(set_3)
+list_4 = list(set_3)
+print(len(list_4))
+
+for i in range(len(list_4)):
+    for k in range(len(list_4)):
+        if list_4[k] > list_4[i]:
+            temp = list_4[k]
+            list_4[k] = list_4[i]
+            list_4[i] = temp
+
+print(list_4)
